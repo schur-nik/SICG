319329b6d2b18b4f8b587d1cc36ff0d0366bbbb4
Shyr_NS <schur.nik@mail.ru>
2021-03-01 16:56:00 +0300

    Start SICG project

---
+++ src/main/java/sample/Conrollers/MenuController.java
@@ -0,0 +1,31 @@
+package sample.Conrollers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController {
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void startButtonAction(ActionEvent event) throws IOException {
+        Stage newStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("LevelsFrame.fxml"));
+        newStage.setTitle("Levels");
+        newStage.setScene(new Scene(root, 440, 670));
+        newStage.show();
+        startButton.getScene().getWindow().hide();
+    }
+
+    @FXML
+    void initialize() {}
+
+}
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MenuFrame.fxml"));
+        primaryStage.setTitle("SICG");
+        primaryStage.setScene(new Scene(root, 440, 670));
         primaryStage.show();
     }
 
