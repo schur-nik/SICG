4ecc1ab1f34bd23d95916e60a3c38daeead72283
Shyr_NS <schur.nik@mail.ru>
2021-05-26 14:13:16 +0300

    2605v2

--- src/projectClient/java/client/Controllers/MenuController.java
+++ src/projectClient/java/client/Controllers/MenuController.java
@@ -2,23 +2,30 @@ package client.Controllers;
 
         import com.aspose.cad.internal.N.G;
         import javafx.event.ActionEvent;
+        import javafx.event.EventHandler;
         import javafx.fxml.FXML;
         import javafx.fxml.FXMLLoader;
         import javafx.geometry.Insets;
         import javafx.scene.Group;
         import javafx.scene.Parent;
+        import javafx.scene.Scene;
         import javafx.scene.SubScene;
         import javafx.scene.control.Button;
         import javafx.scene.control.Label;
         import javafx.scene.image.Image;
         import javafx.scene.image.ImageView;
+        import javafx.scene.input.MouseEvent;
         import javafx.scene.layout.*;
         import javafx.scene.paint.Color;
         import javafx.scene.transform.Scale;
+        import javafx.stage.Stage;
+        import javafx.stage.StageStyle;
 
         import java.io.IOException;
 
 public class MenuController {
+    private double xOffset;
+    private double yOffset;
 
     @FXML
     private Label l1menu;
@@ -45,8 +52,31 @@ public class MenuController {
     private SubScene subsceneOne;
 
     @FXML
-    void buttonAuthAction(ActionEvent event) {
-
+    void buttonAuthAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Models/AuthFrame.fxml"));
+        Stage authStage = new Stage();
+        authStage.initStyle(StageStyle.UNDECORATED);
+        authStage.initStyle(StageStyle.TRANSPARENT);
+        authStage.setTitle("Authentication");
+        Scene authScene = new Scene(root);
+        authScene.setFill(Color.TRANSPARENT);
+        authScene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = authStage.getX() - event.getScreenX();
+                yOffset = authStage.getY() - event.getScreenY();
+            }
+        });
+        authScene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                authStage.setX(event.getScreenX() + xOffset);
+                authStage.setY(event.getScreenY() + yOffset);
+            }
+        });
+        authStage.setScene(authScene);
+        authStage.setResizable(false);
+        authStage.show();
     }
 
     @FXML
@@ -68,11 +98,9 @@ public class MenuController {
     @FXML
     void initialize() {
         BorderPane pane = new BorderPane();
-        //pane.setPrefSize(745, 560);
         pane.setBackground(new Background(new BackgroundFill(Color.rgb(0, 0, 0), CornerRadii.EMPTY, Insets.EMPTY)));
         pane.setCenter(new ImageView(new Image("TaskRes/Images/MenuImages/40iG.gif", 745, 560, true, false)));
         subsceneOne.setRoot(pane);
-
     }
 
 }
--- src/projectClient/java/client/Main.java
+++ src/projectClient/java/client/Main.java
@@ -6,11 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
